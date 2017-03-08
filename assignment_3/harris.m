ae6cd16de6354cb0c9a0fe1ad1860b7a5fb8a2db
MironencoMircea <mircea.mironenco@gmail.com>
2017-03-08 12:47:14 +0200

    Comments, scale derivatives in plot, remove corners in harris

--- assignment_3/harris.m
+++ assignment_3/harris.m
@@ -5,28 +5,41 @@ function [H, r, c] = harris(img, kernel_size, sigma, n, threshold)
  
  % returns the H matrix
  % along with the row and columns of the corners
- 
  grayscale_image = double(rgb2gray(img));
 
+ % By applying a Gaussian filter, convolve at each i,j point
+ % Also compute the gaussian derivative.
  G = fspecial('gaussian', kernel_size, sigma);
  [Gx, Gy] = gradient(G);
+ 
+ % Find derivatives by convolving with image
  Ix = imfilter(grayscale_image, Gx, 'same');
  Iy = imfilter(grayscale_image, Gy, 'same');
  
+ % Construct A,B,C
  A = imfilter(Ix .* Ix, G, 'same');
  B = imfilter(Ix .* Iy, G, 'same');
  C = imfilter(Iy .* Iy, G, 'same');
  
+ % Construct the H matrix.
  H = (A.*C - B.^2) - 0.04 .* (A + C) .^ 2;
 
  % Sliding window to determine max in each neighbourhood
  maxH = imdilate(H, ones(n*2+1));
+ 
  % Equivalent but slower
  % maxH = nlfilter(H, [n*2+1 n*2+1], @(x) max(x(:)));
 
  % Value is good if it's the maximum in that neighbourhood and it's above
  % the threshold
  corners = (H == maxH) & (H > threshold);
+ 
+ % For better illustration, we also remove those
+ % found in the corners of the image.
+ corners(end - n:end, end - n:end) = 0;
+ corners(1:n, end - n:end) = 0;
+ corners(1:n, 1:n) = 0;
+ corners(end - n:end, 1:n) = 0;
  
  % Return row and column indices of corners
  [r, c] = find(corners);
@@ -39,7 +52,7 @@ function [H, r, c] = harris(img, kernel_size, sigma, n, threshold)
 %  subplot(4,1,4),imshow(H==maxH&H>threshold);
 
  figure;
- subplot(131), imshow(Ix);
- subplot(132), imshow(Iy);
- subplot(133), imshow(img); hold on; plot(c,r, 'go');
+ subplot(131), imshow(100 * Ix); title('Derivatives in X direction')
+ subplot(132), imshow(100 * Iy); title('Derivatives in Y direction')
+ subplot(133), imshow(img); hold on; plot(c,r, 'go'); title('Corners')
 end
--- assignment_3/main.m
+++ assignment_3/main.m
@@ -1,6 +1,6 @@
 p = imread('person_toy/00000001.jpg');
 harris(p, 15, 3, 10, 1);
 
-s1 = imread('sphere1.ppm');
-s2 = imread('sphere2.ppm');
-lucas_kanade(s1, s2);
+%s1 = imread('sphere1.ppm');
+%s2 = imread('sphere2.ppm');
+%lucas_kanade(s1, s2);
