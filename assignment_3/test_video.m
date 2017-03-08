f2b19309bbc7a82afd36c0c1e17d066eea823eb6
MironencoMircea <mircea.mironenco@gmail.com>
2017-03-08 19:45:37 +0200

    bug fixing

--- assignment_3/test_video.m
+++ assignment_3/test_video.m
@@ -1,16 +1,10 @@
-fig_id = figure;
-
 kernel_size = 15;
 sigma = 5;
 window_size = 20;
 threshold = 1;
 
 % Get corners from first image.
-[~, r, c] = harris(imread('pingpong/0000.jpeg'), kernel_size, sigma, window_size, threshold);
-
-writer = VideoWriter('person.avi');
-writer.FrameRate = 15;
-open(writer);
+[~, r, c] = harris(imread('pingpong/0000.jpeg'), kernel_size, sigma, window_size, threshold, false);
 
 num_files = 52;
 
@@ -23,7 +17,7 @@ for img = 1:num_files
     % Get optical flow arrows
     [vx, vy] = lucas_kanade_points(img1,img2, r, c, kernel_size);
     
-    figure(fig_id);
+    fig = figure(img);
     axis equal
     imshow(rgb2gray(img1))
     hold on;
@@ -34,13 +28,25 @@ for img = 1:num_files
     hold on;
     
     plot(c,r,'r.','MarkerSize',15)
-    frame = getframe(gcf);
-    writeVideo(writer, frame);
-    hold off
-    
+    file = ['pingpong_frames/' num2str(img) '.png'];
+%     set(fig, 'Position', [0 0 1167 875])
+    saveas(fig, file);
+    pause(0.8);
+    close(fig);    
     
     r = int16(r) + int16(2.9 * vx');
     c = int16(c) + int16(1.5 * vy');
+end
+
+
+writer = VideoWriter('pingpong.avi');
+writer.FrameRate = 10;
+open(writer);
+
+for file_index = 1:num_files
+    file = ['pingpong_frames/' num2str(file_index,'%d') '.png'];
+    frame = imread(file);
+    writeVideo(writer, frame);
 end
 
 close(writer);
