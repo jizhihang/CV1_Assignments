405d428c0ab1199a69281f0b72349d769dae39d2
MironencoMircea <mircea.mironenco@gmail.com>
2017-03-30 20:08:32 +0200

    added color conversion

---
+++ final_assignment/color_convert.m
@@ -0,0 +1,31 @@
+function [ outIm ] = color_convert(inIm, type)
+
+switch type
+    case 'RGB2hsv'
+        outIm = rgb2hsv(inIm);
+    case 'RGB2opponent'
+        % Get individual channels
+        R = inIm(:,:,1);
+        G = inIm(:,:,2);
+        B = inIm(:,:,3);
+        
+        % Opponent channels
+        O1 = (R - G) ./ sqrt(2.);
+        O2 = (R + G - 2 * B) ./ sqrt(6.);
+        O3 = (R + G + B) ./ sqrt(3.);
+        
+        % Set new channels
+        outIm(:,:,1) = O1;
+        outIm(:,:,2) = O2;
+        outIm(:,:,3) = O3;
+    case 'RGB2rgb'
+        norm = inIm(:,:,1) + inIm(:,:,2) + inIm(:,:,3);
+        
+        % Normalize
+        outIm(:,:,1) = inIm(:,:,1) ./ norm;
+        outIm(:,:,1) = inIm(:,:,2) ./ norm;
+        outIm(:,:,1) = inIm(:,:,3) ./ norm;
+    otherwise
+        throw(MException('SIFT:COLOR', 'Color must be RGB2hsv, RGB2opponent or RGB2rgb'));
+end
+
